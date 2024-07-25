b2da0934b1935112f6cafd8d339b6cf8b3a57276
Joshua Potter <jrpotter2112@gmail.com>
2024-07-25 14:09:01 -0600

    Add tailwind.

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config}*/
+module.exports = {
+  content: ["./entrypoints/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      prefix: "fluidic-workflows-",
+    },
+  },
+  plugins: [],
+}
