0b46e6f30a2883ba77a748f87882b0081334c79e
Joshua Potter <jrpotter2112@gmail.com>
2024-07-26 06:18:28 -0600

    Keep top-level tabs rendered.

--- entrypoints/sidepanel/App.tsx
+++ entrypoints/sidepanel/App.tsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 import BuilderTab from "./BuilderTab"
@@ -5,20 +7,22 @@ import LibraryTab from "./LibraryTab"
 import RunnerTab from "./RunnerTab"
 
 function App() {
+  const [tabValue, setTabValue] = React.useState("builder")
+
   return (
-    <Tabs defaultValue="builder">
+    <Tabs value={tabValue} onValueChange={setTabValue}>
       <TabsList className="grid grid-cols-3">
         <TabsTrigger value="builder">Builder</TabsTrigger>
         <TabsTrigger value="library">Library</TabsTrigger>
         <TabsTrigger value="runner">Runner</TabsTrigger>
       </TabsList>
-      <TabsContent value="builder">
+      <TabsContent value="builder" forceMount hidden={tabValue !== "builder"}>
         <BuilderTab />
       </TabsContent>
-      <TabsContent value="library">
+      <TabsContent value="library" forceMount hidden={tabValue !== "library"}>
         <LibraryTab />
       </TabsContent>
-      <TabsContent value="runner">
+      <TabsContent value="runner" forceMount hidden={tabValue !== "runner"}>
         <RunnerTab />
       </TabsContent>
     </Tabs>
