86eea2b847b5539d1242a0953ade866e1154ec21
Joshua Potter <jrpotter2112@gmail.com>
2024-08-06 13:51:51 -0600

    Move to consolidated store.

--- entrypoints/sidepanel/store/runner_slice.ts
+++ entrypoints/sidepanel/store/runner_slice.ts
@@ -1,20 +1,62 @@
-import type { Workflow } from "@/utils/workflow"
+import { type Workflow, StepResult } from "@/utils/workflow"
 import { type SharedStateCreator } from "./index"
 
 export type RunnerSlice = {
   runnerActive: Workflow | null
+  runnerTabId: number
+  runnerTaskIndex: number
+  runnerStepIndex: number
+  runnerResults: StepResult[]
 
   runnerActions: {
     startWorkflow: (workflow: Workflow) => void
+    isFinished: () => boolean
+    getParams: () => Map<string, string>
+    getTaskResult: () => TaskResult | null
+    getStepResult: () => StepResult | null
   }
 }
 
-export const runnerSlice: SharedStateCreator<RunnerSlice> = (set) => ({
+export const runnerSlice: SharedStateCreator<RunnerSlice> = (set, get) => ({
   runnerActive: null,
+  runnerTabId: -1,
+  runnerTaskIndex: -1,
+  runnerStepIndex: -1,
+  runnerResults: [],
 
   runnerActions: {
     startWorkflow: (workflow) => {
       set({ sharedActiveTab: "runner", runnerActive: workflow })
+    },
+
+    isFinished: () => {
+      const active = get().runnerActive
+      if (!active) {
+        return false
+      }
+      return get().runnerStepIndex >= active.steps.length
+    },
+
+    getParams: () => {
+      const params = new Map()
+      get().runnerResults.forEach((result) => {
+        for (const [key, val] of result.params.entries()) {
+          params.set(key, val)
+        }
+      })
+      return params
+    },
+
+    getTaskResult: () => {
+      return (
+        get().runnerResults[get().runnerStepIndex]?.tasks[
+          get().runnerTaskIndex
+        ] ?? null
+      )
+    },
+
+    getStepResult: () => {
+      return get().runnerResults[get().runnerStepIndex] ?? null
     },
   },
 })
