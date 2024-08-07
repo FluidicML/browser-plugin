6d00e935236ca008c8a692b763b221f8fada917d
Joshua Potter <jrpotter2112@gmail.com>
2024-08-07 14:01:36 -0600

    Add default workflow name.

--- entrypoints/sidepanel/builder_tab/InitTabPanel.tsx
+++ entrypoints/sidepanel/builder_tab/InitTabPanel.tsx
@@ -13,6 +13,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { useSharedStore } from "../store"
 
 type InitTabPanelProps = Omit<
   React.ComponentPropsWithoutRef<typeof TabsContent>,
@@ -26,16 +27,24 @@ const InitTabPanel = React.forwardRef<
   React.ElementRef<typeof TabsContent>,
   InitTabPanelProps
 >(({ defaultValues, onChange, ...props }, ref) => {
+  const store = useSharedStore()
+
   const form = useForm<InitSchema>({
     resolver: zodResolver(initSchema),
-    defaultValues: defaultValues ?? { name: "" },
+    defaultValues: defaultValues ?? {
+      name: `Untitled #${store.librarySaved.length + 1}`,
+    },
   })
 
   React.useEffect(() => {
+    const parsed = initSchema.safeParse(form.getValues())
+    onChange(parsed.success ? parsed.data : null)
+
     const subscription = form.watch((values) => {
       const parsed = initSchema.safeParse(values)
       onChange(parsed.success ? parsed.data : null)
     })
+
     return () => subscription.unsubscribe()
   }, [form.watch])
 
