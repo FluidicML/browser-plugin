a93eb18bcd70f22bbf40ba571747d1e804799cc4
morganhowell95 <morgan.howell95@gmail.com>
2024-08-07 19:16:46 -0700

    chore: rm api-client build target to separate PR

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "wxt"
-import viteConfig from "./vite.config"
+import tailwindcss from "tailwindcss"
+import autoprefixer from "autoprefixer"
+import { resolve } from "path"
 
+// See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   manifest: {
@@ -8,7 +11,24 @@ export default defineConfig({
     permissions: ["storage", "tabs", "activeTab", "scripting"],
     host_permissions: ["<all_urls>"],
   },
-  vite: () => viteConfig,
+  vite: () => ({
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
+      },
+    },
+    resolve: {
+      alias: {
+        $: resolve(__dirname, "src"),
+      },
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".css"],
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        target: "esnext",
+      },
+    },
+  }),
   outDir: "dist",
   entrypointsDir: "src/entrypoints",
 })
